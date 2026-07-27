84e394864523312920f016a060cdede43b88a067
Amaury TISSOT <tissot.amaury@gmail.com>
2026-07-27 11:59:13 +0200

    Nomme explicitement le container PostgreSQL microservice_rgpd_bdd

--- src/MicroserviceRgpd.AspireHost/AppHost.cs
+++ src/MicroserviceRgpd.AspireHost/AppHost.cs
@@ -2,6 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add PostgreSQL container
 var postgres = builder.AddPostgres("postgres")
+  .WithContainerName("microservice_rgpd_bdd")
   .WithLifetime(ContainerLifetime.Persistent)
   .WithDataVolume();
 
